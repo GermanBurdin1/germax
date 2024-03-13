086aa66b66c373fcd1cbda089a881fe465174975
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-03-13 20:58:29 +0100

    homepage. version intermédiaire

---
+++ app/views/shared/footer.html.php
@@ -0,0 +1,39 @@
+<footer class="bg-dark text-white py-5">
+    <div class="container">
+        <div class="row text-center mb-4">
+            <div class="col">
+                <h2 class="text-uppercase">LOC MNS</h2>
+            </div>
+        </div>
+        
+        <div class="row justify-content-center mb-4">
+            <div class="col-auto">
+                <a href="http://example.com/about" class="btn btn-outline-light text-uppercase" role="button">À propos</a>
+            </div>
+            <div class="col-auto">
+                <a href="http://example.com/privacy" class="btn btn-outline-light text-uppercase" role="button">Politique de confidentialité</a>
+            </div>
+            <div class="col-auto">
+                <a href="http://example.com/contact" class="btn btn-outline-light text-uppercase" role="button">Contacts</a>
+            </div>
+        </div>
+        
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                <a href="http://vk.com/groupname" class="btn btn-outline-light" role="button" aria-label="VK">
+                    <i class="fab fa-vk"></i>
+                </a>
+            </div>
+            <div class="col-auto">
+                <a href="http://facebook.com/groupname" class="btn btn-outline-light" role="button" aria-label="Facebook">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+            </div>
+            <div class="col-auto">
+                <a href="http://twitter.com/groupname" class="btn btn-outline-light" role="button" aria-label="Twitter">
+                    <i class="fab fa-twitter"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
